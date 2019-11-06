8cb0618059957779bbd07f77dd7736e6b1834007
Gaurav Sharma <gauravsharma10167@gmail.com>
2019-11-07 00:33:55 +0530

    First commit

---
+++ implemetation of stack.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+int stack[100], choice, n, top, x, i;
+void push(void);
+void pop(void);
+void display(void);
+int main()
+{
+    top = -1;
+    printf("Enter size of stack[MAX=100]: ");
+    scanf("%d", &n);
+    printf("\n\t STACK OPERATIONS USING ARRAY");
+    printf("\n\t 1. PUSH \n\t 2. POP \n\t 3. DISPLAY \n\t 4. EXIT");
+    do
+    {
+        printf("Enter the choice: ");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+        {
+            push();
+            break;
+        }
+        case 2:
+        {
+            pop();
+            break;
+        }
+        case 3:
+        {
+            display();
+            break;
+        }
+        case 4:
+        {
+            printf("\n\t EXIT POINT ");
+            break;
+        }
+        default:
+        {
+            printf("\n\t Please enter valid choice ");
+        }
+        }
+    } while (choice != 4);
+    return 0;
+}
+
+void push()
+{
+    if (top >= n - 1)
+        printf("Stack Overflow\n");
+    else
+    {   
+        printf("Enter value to be pushed");
+        scanf("%d", &n);
+        top++;
+        stack[top] = n;
+    }
+}
+void pop()
+{
+    if (top <= -1)
+       printf("Stack Underflow\n");
+    else
+    {
+        printf("The popped element is %d\n" , stack[top]);
+        top--;
+    }
+}
+void display()
+{
+    if (top >= 0)
+    {
+        printf("Stack elements are:\n");
+        for (i = top; i >= 0; i--)
+            printf("%d ", stack[i]);
+        printf("\n Press next choice");
+    }
+    else
+        printf("The STACK is empty");
+}
---
+++ infixtopostfix.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 2000
+struct Stack
+{
+    int top;
+    char arr[MAX];
+};
+
+struct Stack* createStack(){
+    struct Stack *stack = (struct Stack*)malloc(sizeof(struct Stack)); 
+    stack->top = -1;
+    return stack;
+}
+void push(struct Stack *stack, int x)
+{   
+    if (stack->top == MAX - 1)
+    {
+        printf("Stack Overflow");
+        exit(1);
+    }
+    stack->arr[++(stack->top)] = x;
+}
+
+int pop(struct Stack *stack)
+{
+    if (stack->top == -1)
+    {
+        printf("Stack underflow");
+        exit(1);
+    }
+    return stack->arr[(stack->top)--];
+}
+int peek(struct Stack *stack){
+    return (stack->arr[stack->top]);
+}
+int isEmpty(struct Stack* stack){
+    return stack->top == -1;
+}
+void display(struct Stack *stack)
+{
+    int pos = 0;
+    while (pos <= stack->top)
+    {
+        printf("%d ", stack->arr[pos]);
+        pos++;
+    }
+}
+int preced(char operand){
+    if(operand == '+' || operand == '-')
+        return 1;
+    else if(operand == '*' || operand == '/')
+        return 2;
+    else if(operand == '$')
+        return 3;
+    else
+        return -1;
+}
+int isoperand(char ch){
+    if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+        return 1;
+    return 0;
+}
+int infixtopostfix(char *infix){
+    struct Stack *stack = createStack();
+    int i,k;
+    if(!stack) 
+        return -1 ; 
+    char post[MAX];
+    for(i = 0, k = -1; infix[i] != '\0'; i++){
+        char ch = infix[i];
+        if(isoperand(ch) == 1){
+            post[++k] = ch;
+        }
+        else if(ch == '(')
+            push(stack, ch);
+        else if(ch == ')'){
+            while(!isEmpty(stack) && peek(stack) != '('){
+                post[++k] = pop(stack);
+            }
+            if (!isEmpty(stack) && peek(stack) != '(') 
+                return -1;            
+            else
+                pop(stack); 
+        }
+        else{
+            while(!isEmpty(stack) && (preced(ch) <= preced(peek(stack)))){
+                post[++k] = pop(stack);
+            }
+            push(stack, ch);
+        }
+    }
+    while(!isEmpty(stack)){
+        post[++k] = pop(stack);
+    }
+    post[++k] = '\0';
+    printf("%s", post);
+
+}
+int main()
+{   
+    struct Stack *stack = createStack();
+    char infix[] = "a+b*(c$d-e)$(f+g*h)-i\0";
+    infixtopostfix(infix);
+    return 0;
+}
